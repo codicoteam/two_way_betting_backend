8c44020ece9eb6b72adf253b9ce64afb6bbd9b4a
Takunda Gowa <takundagowa@gmail.com>
2026-04-27 09:07:24 +0200

    chore: allow all origins in CORS config

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -1,15 +1,8 @@
 const env = require('./env');
 
+
 const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests from frontend and mobile apps
-    const allowedOrigins = [env.FRONTEND_URL];
-    if (!origin || allowedOrigins.includes(origin) || env.NODE_ENV === 'development') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200,
 };
